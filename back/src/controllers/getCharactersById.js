ce09118927823c55af1e91055f6fdf1fdcd95248
Seb-RM <srmmoto@gmail.com>
2023-11-06 22:46:51 -0300

    Etapa async await done

--- back/src/controllers/getCharactersById.js
+++ back/src/controllers/getCharactersById.js
@@ -5,28 +5,27 @@ const  axios  = require('axios');
 //const URL = `https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`; se debería usar separando la url
 //const URL = 'https://rickandmortyapi.com/api/character'
 
-const getCharById = (request, response)=> {
-    const { id} = request.params;
+const getCharById = async (request, response)=> {
+    // const { id} = request.params;
 
-    axios.get(`https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`)
+    // axios.get(`https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`)
         //.then(({ data }) => {
-            .then((response)=> response.data)
-            
-            .then(({name,status,origin,image,gender,species})=>{
-                if(id){
-                    const character = {
-                                    id,
-                                    name,
-                                    status,
-                                    origin,
-                                    image,
-                                    gender,
-                                    species
-                                };
-                    return response.status(200).json(character);
-                }
-                return response.status(404).send("Not found");
-            })
+            // .then((response)=> response.data)
+            // .then(({name,status,origin,image,gender,species})=>{
+            //     if(id){
+            //         const character = {
+            //                         id,
+            //                         name,
+            //                         status,
+            //                         origin,
+            //                         image,
+            //                         gender,
+            //                         species
+            //                     };
+            //         return response.status(200).json(character);
+            //     }
+            //     return response.status(404).send("Not found");
+            // })
         //     if (data.id) {
         //     const character = {
         //         id,
@@ -42,9 +41,30 @@ const getCharById = (request, response)=> {
         //     }
         //     return response.status(404).send("Not found");
         // })
-        .catch((error) => {
-            return response.status(500).send(error.message);
-        });
+        // .catch((error) => {
+        // //     return response.status(500).send(error.message);
+        // });
+    try {
+        const {id} = request.params;
+        const {data} = await axios.get(
+            `https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`
+        );
+
+        const character = {
+                id,
+                name: data.name,
+                status: data.status,
+                origin: data.origin,
+                image: data.image,
+                gender: data.gender,
+                species: data.species,
+            };
+
+        return response.status(200).json(character);
+
+    } catch (error) {
+        return response.status(500).send(error.message);
+    }
 };
 
 module.exports ={
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -37,14 +37,28 @@ function App() {
 
   if (pathname === "/about") clase = "App about";
 
-const  login=(userData)=> {
-  const { email, password } = userData;
-  const URL = "http://localhost:3001/rickandmorty/login/";
-  axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+const  login= async (userData)=> {
+  // const { email, password } = userData;
+  // const URL = "http://localhost:3001/rickandmorty/login/";
+  // axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+  //   const { access } = data;
+  //   setAccess(data);
+  //   access && navigate("/home");
+  // });
+  try {
+    const { email, password } = userData;
+    const URL = "http://localhost:3001/rickandmorty/login/";
+    const {data} = await axios(URL + `?email=${email}&password=${password}`)
+
     const { access } = data;
-    setAccess(data);
+
+    setAccess(access);
+
     access && navigate("/home");
-  });
+
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 
@@ -64,7 +78,7 @@ const  login=(userData)=> {
     };
     
   //(...characters,id) tambien se puede usar de esta forma
-  const onSearch = (id) => {
+  const onSearch = async (id) => {
     // fetch(`https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`)
     // .then((response)=> response.json())
     // .then( (data ) => {
@@ -75,23 +89,46 @@ const  login=(userData)=> {
     //     }
     //   }
     // );
-    if (characterSet.has(id)) {
-      window.alert(
-        "¡Este personaje ya se encuentra seleccionado, intenta otra vez!"
-      );
-    } else {
-      axios(
-        //`https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`
-        `http://localhost:3001/rickandmorty/character/${id}`
-      ).then(({ data }) => {
-        if (data.name) {
-          setCharacters((oldChars) => [...oldChars, data]);
-          setCharacterSet(new Set([...characterSet, id]));
+    console.log(characterSet);
+        if (characterSet.has(id)) {
+
+          window.alert(
+            "¡Este personaje ya se encuentra seleccionado, intenta otra vez!"
+          );
         } else {
-          window.alert("¡No hay personajes con este ID!");
+          try {
+            const { data } = await axios(
+              `http://localhost:3001/rickandmorty/character/${id}`
+            );
+
+            if (data.name) {
+              setCharacters((oldChars) => [...oldChars, data]);
+              setCharacterSet(new Set([...characterSet, id]));
+            } else {
+              window.alert("¡No hay personajes con este ID!");
+            }
+          } catch (error) {
+            window.alert(error);
+          }
         }
-      });
-    }
+    
+    // if (characterSet.has(id)) {
+    //   window.alert(
+    //     "¡Este personaje ya se encuentra seleccionado, intenta otra vez!"
+    //   );
+    // } else {
+    //   axios(
+    //     //`https://rym2.up.railway.app/api/character/${id}?key=pi-seb-rm`
+    //     `http://localhost:3001/rickandmorty/character/${id}`
+    //   ).then(({ data }) => {
+    //     if (data.name) {
+    //       setCharacters((oldChars) => [...oldChars, data]);
+    //       setCharacterSet(new Set([...characterSet, id]));
+    //     } else {
+    //       window.alert("¡No hay personajes con este ID!");
+    //     }
+    //   });
+    // }
   };
 
   const onClose = (id) => {
