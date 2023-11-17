8b2b883df7147fbf00033cb47b85447edb80560d
Seb-RM <srmmoto@gmail.com>
2023-11-16 21:48:04 -0300

    Sequelize parte 2 done

--- back/src/controllers/login.js
+++ back/src/controllers/login.js
@@ -1,14 +1,34 @@
-const users = require('../utils/user')
+// const users = require('../utils/user')
 
-const login = (request, response)=>{
-    const {email, password} = request.query;
-    const userFound = users.find((user)=> user.email === email && user.password === password);
+// const login = (request, response)=>{
+//     const {email, password} = request.query;
+//     const userFound = users.find((user)=> user.email === email && user.password === password);
 
-    if(userFound) return response.status(200).json({'access': true});
+//     if(userFound) return response.status(200).json({'access': true});
 
-    return response.status(404).json({'access': false});
-};
+//     return response.status(404).json({'access': false});
+// };
 
-module.exports ={
-    login
+// module.exports ={
+//     login
+// }
+const {User} = require("../DB_connection");
+
+module.exports = async(req, res)=>{
+    try {
+        const { email, password } = req.query;
+
+        if(!email || !password) return res.status(400).send("Faltan datos.");
+
+        const user = await User.findOne({where:{email}});
+
+        if(!user) return res.status(404).send("Usuario no encontrado.");
+
+        return user.password === password
+            ? res.json({access: true})
+            : res.status(403).send("Contraseña Incorrecta.");
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 }
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,6 +1,10 @@
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
-const { login } = require("../controllers/login");
+// const { postFav, deleteFav } = require("../controllers/handleFavorites");
+// const { login } = require("../controllers/login");
 const { getCharById } = require("../controllers/getCharactersById");
+const login = require('../controllers/login');
+const postUser = require('../controllers/postUser');
+const postFav = require('../controllers/postFavs');
+const deleteFav = require('../controllers/deleteFav');
 
 const express = require("express");
 const router = express.Router();
@@ -8,6 +12,7 @@ const router = express.Router();
 router.get("/character/:id", getCharById);
 
 router.get("/login", login);
+router.post("/login", postUser);
 
 router.post("/fav", postFav);
 
