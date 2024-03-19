0f9ce9f4caed3f59eaa369b1490bc32d71e1b561
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-03-18 21:16:59 -0300

    cambios en los links de la seccion about ahora si redirigen a las paginas reales

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import styles from './About.module.css'
+import  { Link} from "react-router-dom"
 
 import "boxicons";
 
@@ -6,39 +7,54 @@ const About =()=>{
     return (
         <div>
             <div className={styles.contenedor}>
-            <h2>Esta Web App fue desarrollada por:</h2>
-            <div className={styles.titulo}>
-                <h1>Seb-RM</h1>
-            </div>
-            <div className={styles.links}>
+                <h2>Esta Web App fue desarrollada por:</h2>
+                <div className={styles.titulo}>
+                    <h1>Seb-RM</h1>
+                </div>
+                <div className={styles.links}>
                 <h3>Links to:</h3>
                 <span className={styles.icons}>
-                <box-icon
-                    type="logo"
-                    name="github"
-                    animation="tada-hover"
-                    color="#67cfdd"
-                    size="sm"></box-icon>
-                <box-icon
-                    type="logo"
-                    name="linkedin-square"
-                    animation="tada-hover"
-                    color="#67cfdd"
-                    size="sm"></box-icon>
-                <box-icon
-                    type="logo"
-                    name="gmail"
-                    animation="tada-hover"
-                    color="#67cfdd"
-                    size="sm"></box-icon>
-                <box-icon
-                    name="twitter"
-                    type="logo"
-                    animation="tada-hover"
-                    color="#67cfdd"
-                    size="sm"></box-icon>
+                    <Link
+                    to={"https://github.com/Seb-RM"}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <box-icon
+                        type="logo"
+                        name="github"
+                        animation="tada-hover"
+                        color="#67cfdd"
+                        size="sm"></box-icon>
+                    </Link>
+                    <Link
+                    to={"https://www.linkedin.com/in/sebastian-rodriguez-moure"}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <box-icon
+                        type="logo"
+                        name="linkedin-square"
+                        animation="tada-hover"
+                        color="#67cfdd"
+                        size="sm"></box-icon>
+                    </Link>
+                    {/* <Link
+                    to={"mailto:mouresebastian@yahoo.com.ar"}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <box-icon
+                        type="solid"
+                        name="envelope"
+                        animation="tada-hover"
+                        color="#67cfdd"
+                        size="sm"></box-icon>
+                    </Link> */}
+                    {/* <box-icon
+                        name="twitter"
+                        type="logo"
+                        animation="tada-hover"
+                        color="#67cfdd"
+                        size="sm"></box-icon> */}
                 </span>
-            </div>
+                </div>
             </div>
         </div>
     );
